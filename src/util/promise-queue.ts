b237227bb89216a2611fbd525e1fbce388e2a4a1
Carita Logrén <carita.logren@gmail.com>
2018-11-10 22:49:10 +0200

    Create a Queue class that allows enqueuing functions that must be run serially.

---
+++ src/util/promise-queue.ts
@@ -0,0 +1,78 @@
+import { isUndefined } from 'util';
+import { logger } from './logger';
+
+/** Configuration options for the promise queue */
+export interface PromiseQueueOptions {
+  /** Enables or disables automatic execution of the queue
+   * once new functions are added to the queue. Defaults to `true`*/
+  autorun?: boolean;
+  /** Enables setting a maximum frequency on how often the
+   * functions can be executed. Defaults to `0`.*/
+  interval?: number;
+}
+
+/** This class enables queuing functions to be executed serially. */
+export class PromiseQueue {
+  private running: boolean = false;
+  private autorun: boolean;
+  private interval: number;
+  private queue: (Function)[] = [];
+
+  /**
+   * Create a new promise queue
+   * @param options Configuration options for the queue.
+   */
+  constructor(options?: PromiseQueueOptions) {
+    this.autorun =
+      options && !isUndefined(options.autorun) ? options.autorun : true;
+    this.interval = (options && options.interval) || 0;
+  }
+
+  private async push(callback: Function) {
+    this.queue.push(callback);
+    if (this.autorun && !this.running) {
+      this.dequeue();
+    }
+  }
+
+  /**
+   * Adds the given function to the queue and returns a promise
+   * that will be resolved to the return value of the function
+   * once it's executed.
+   * @param fun Function to enqueue.
+   */
+  async enqueue(fun: Function) {
+    return new Promise(async (resolve, reject) => {
+      const resolver = async () => {
+        try {
+          const ret = await fun();
+          resolve(ret);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      this.push(resolver).catch((e) => logger.error(e));
+    });
+  }
+
+  /**
+   * Start dequeuing. If option `autorun` is used, this function
+   * does not need to be called manually.
+   */
+  async dequeue() {
+    this.running = true;
+    const func = this.queue.shift();
+    if (func) {
+      func();
+    }
+
+    await new Promise((resolve) => setTimeout(() => resolve(), this.interval));
+
+    if (this.queue.length > 0) {
+      this.dequeue();
+    } else {
+      this.running = false;
+    }
+  }
+}
