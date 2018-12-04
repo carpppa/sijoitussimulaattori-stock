7fe0d01d674da201d11802c370cbcfa94e8658b1
Carita Logrén <carita.logren@gmail.com>
2018-12-04 22:20:02 +0200

    Convert 'ready' listener to listen once, remove error listener.

--- src/redis.ts
+++ src/redis.ts
@@ -4,7 +4,6 @@ import * as redis from 'redis';
 
 const client = redis.createClient(config.redis.REDIS_URL);
 
-client.on('error', (err) => logger.debug('redis error', err));
-client.on('ready', () => logger.info('redis connection ready'));
+client.once('ready', () => logger.info('redis connection ready'));
 
 export { client };
