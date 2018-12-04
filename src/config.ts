e12ed763f267b621b4236469ac5f4344bbaf328c
Carita Logrén <carita.logren@gmail.com>
2018-12-04 20:20:59 +0200

    Fetch alpha vantage url always from environment.

--- src/config.ts
+++ src/config.ts
@@ -5,16 +5,18 @@ process.on('uncaughtException', (err) => {
 });
 
 // List here the required environment variables:
-ensureNecessaryEnvs(['NODE_ENV', 'DATABASE_URL', 'ALPHA_VANTAGE_API_KEY']);
+ensureNecessaryEnvs([
+  'NODE_ENV',
+  'DATABASE_URL',
+  'ALPHA_VANTAGE_URL',
+  'ALPHA_VANTAGE_API_KEY',
+]);
 
 const config = {
   app: {
     NODE_ENV: process.env.NODE_ENV || 'development',
     LOGGER_LEVEL: process.env.LOG_LEVEL || 'info',
-    ALPHA_VANTAGE_URL:
-      (process.env.NODE_ENV !== 'development' &&
-        process.env.ALPHA_VANTAGE_URL) ||
-      'http://127.0.0.1:8888',
+    ALPHA_VANTAGE_URL: process.env.ALPHA_VANTAGE_URL || '',
     ALPHA_VANTAGE_API_KEY: process.env.ALPHA_VANTAGE_API_KEY || '',
     CI: process.env.TRAVIS || false,
   },
